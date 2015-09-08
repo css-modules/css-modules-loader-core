6ed995132f141cb03511dd62b8f33de03e330faf
davidmarkclements <huperekchuno@googlemail.com>
2015-09-08 21:47:18 +0100

    sync api

---
+++ src/sync/index.js
@@ -0,0 +1,28 @@
+import postcss from 'postcss'
+import localByDefault from 'postcss-modules-local-by-default'
+import extractImports from 'postcss-modules-extract-imports'
+import scope from 'postcss-modules-scope'
+
+import Parser from './parser'
+
+export default class Core {
+  constructor( plugins ) {
+    this.plugins = plugins || Core.defaultPlugins
+  }
+
+  load( sourceString, sourcePath, trace, pathFetcher ) {
+    let parser = new Parser( pathFetcher, trace )
+
+    const result = postcss( this.plugins.concat( [parser.plugin] ) )
+      .process( sourceString, { from: "/" + sourcePath } )
+
+    return { injectableSource: result.css, exportTokens: parser.exportTokens }
+  }
+}
+
+
+// These three plugins are aliased under this package for simplicity.
+Core.localByDefault = localByDefault
+Core.extractImports = extractImports
+Core.scope = scope
+Core.defaultPlugins = [localByDefault, extractImports, scope]
--- test/test-cases.js
+++ test/test-cases.js
@@ -3,7 +3,9 @@
 import assert from "assert"
 import fs from "fs"
 import path from "path"
-import FileSystemLoader from "../src/file-system-loader"
+import AsyncFileSystemLoader from "../src/async/file-system-loader"
+import SyncFileSystemLoader from "../src/sync/file-system-loader"
+
 
 let normalize = ( str ) => {
   return str.replace( /\r\n?/g, "\n" );
@@ -19,9 +21,9 @@ Object.keys( pipelines ).forEach( dirname => {
     let testDir = path.join( __dirname, dirname )
     fs.readdirSync( testDir ).forEach( testCase => {
       if ( fs.existsSync( path.join( testDir, testCase, "source.css" ) ) ) {
-        it( "should " + testCase.replace( /-/g, " " ), done => {
+        it( "should " + testCase.replace( /-/g, " " ) + '(async)', done => {
           let expected = normalize( fs.readFileSync( path.join( testDir, testCase, "expected.css" ), "utf-8" ) )
-          let loader = new FileSystemLoader( testDir, pipelines[dirname] )
+          let loader = new AsyncFileSystemLoader( testDir, pipelines[dirname] )
           let expectedTokens = JSON.parse( fs.readFileSync( path.join( testDir, testCase, "expected.json" ), "utf-8" ) )
           loader.fetch( `${testCase}/source.css`, "/" ).then( tokens => {
             assert.equal( loader.finalSource, expected )
@@ -34,14 +36,14 @@ Object.keys( pipelines ).forEach( dirname => {
 } )
 
 // special case for testing multiple sources
-describe( 'multiple sources', () => {
+describe( 'multiple sources async', () => {
   let testDir = path.join( __dirname, 'test-cases' )
   let testCase = 'multiple-sources';
   let dirname = 'test-cases';
   if ( fs.existsSync( path.join( testDir, testCase, "source1.css" ) ) ) {
-    it( "should " + testCase.replace( /-/g, " " ), done => {
+    it( "should " + testCase.replace( /-/g, " " ) + '(async)', done => {
       let expected = normalize( fs.readFileSync( path.join( testDir, testCase, "expected.css" ), "utf-8" ) )
-      let loader = new FileSystemLoader( testDir, pipelines[dirname] )
+      let loader = new AsyncFileSystemLoader( testDir, pipelines[dirname] )
       let expectedTokens = JSON.parse( fs.readFileSync( path.join( testDir, testCase, "expected.json" ), "utf-8" ) )
       loader.fetch( `${testCase}/source1.css`, "/" ).then( tokens1 => {
         loader.fetch( `${testCase}/source2.css`, "/" ).then( tokens2 => {
@@ -50,6 +52,46 @@ describe( 'multiple sources', () => {
           assert.equal( JSON.stringify( tokens ), JSON.stringify( expectedTokens ) )
         } ).then( done, done )
       })
+    } );
+  }
+} );
+
+
+
+Object.keys( pipelines ).forEach( dirname => {
+  describe( dirname, () => {
+    let testDir = path.join( __dirname, dirname )
+    fs.readdirSync( testDir ).forEach( testCase => {
+      if ( fs.existsSync( path.join( testDir, testCase, "source.css" ) ) ) {
+        it( "should " + testCase.replace( /-/g, " " ) + '(sync)', () => {
+          let expected = normalize( fs.readFileSync( path.join( testDir, testCase, "expected.css" ), "utf-8" ) )
+          let loader = new SyncFileSystemLoader( testDir, pipelines[dirname] )
+          let expectedTokens = JSON.parse( fs.readFileSync( path.join( testDir, testCase, "expected.json" ), "utf-8" ) )
+          let tokens = loader.fetch( `${testCase}/source.css`, "/" )
+          assert.equal( loader.finalSource, expected )
+          assert.equal( JSON.stringify( tokens ), JSON.stringify( expectedTokens ) )
+
+        } );
+      }
+    } );
+  } );
+} )
+
+// special case for testing multiple sources
+describe( 'multiple sources async', () => {
+  let testDir = path.join( __dirname, 'test-cases' )
+  let testCase = 'multiple-sources';
+  let dirname = 'test-cases';
+  if ( fs.existsSync( path.join( testDir, testCase, "source1.css" ) ) ) {
+    it( "should " + testCase.replace( /-/g, " " ) + '(sync)', () => {
+      let expected = normalize( fs.readFileSync( path.join( testDir, testCase, "expected.css" ), "utf-8" ) )
+      let loader = new SyncFileSystemLoader( testDir, pipelines[dirname] )
+      let expectedTokens = JSON.parse( fs.readFileSync( path.join( testDir, testCase, "expected.json" ), "utf-8" ) )
+      let tokens1 = loader.fetch( `${testCase}/source1.css`, "/" )
+      let tokens2 = loader.fetch( `${testCase}/source2.css`, "/" )
+      assert.equal( loader.finalSource, expected )
+      const tokens = Object.assign({}, tokens1, tokens2);
+      assert.equal( JSON.stringify( tokens ), JSON.stringify( expectedTokens ) )
     } );
   }
 } );
