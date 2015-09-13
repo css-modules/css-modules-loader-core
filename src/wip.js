2e294ae9b523b517d7557bbe3b1247976f569063
Glen Maddern <glenmaddern@gmail.com>
2015-09-13 18:56:03 +1000

    adding replacements for declaration values

--- src/wip.js
+++ src/wip.js
@@ -1,11 +1,10 @@
 const matchImports = /^(.+?)\s+from\s+(?:"([^"]+)"|'([^']+)')$/
 const matchLet = /(?:,\s+|^)([\w-]+):?\s+("[^"]*"|'[^']*'|[^,]+)\s?/g
+const matchConstName = /[\w-]+/g
 
 export default css => {
   let declarations = {}
   css.eachAtRule(/^-?let$/, atRule => {
-    console.log(atRule.params)
-
     let matches
     while (matches = matchLet.exec(atRule.params)) {
       let [/*match*/, key, value] = matches
@@ -18,6 +17,17 @@ export default css => {
       //console.log(imports)
     } else {
       console.log(atRule.params)
+    }
+  })
+  /* Perform replacements */
+  css.eachDecl(decl => {
+    let matches
+    while (matches = matchConstName.exec(decl.value)) {
+      let replacement = declarations[matches[0]]
+      if (replacement) {
+        decl.value = decl.value.slice(0, matches.index) + replacement + decl.value.slice(matchConstName.lastIndex)
+        matchConstName.lastIndex -= matches[0].length - replacement.length
+      }
     }
   })
 }
