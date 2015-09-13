514e8ace7e114ba463244fdf57408da1778733a5
Glen Maddern <glenmaddern@gmail.com>
2015-09-13 18:08:01 +1000

    think my regex fu is ok

--- src/wip.js
+++ src/wip.js
@@ -1,10 +1,21 @@
 const matchImports = /^(.+?)\s+from\s+(?:"([^"]+)"|'([^']+)')$/
+const matchLet = /(?:,\s+|^)([\w-]+):?\s+("[^"]*"|'[^']*'|[^,]+)\s?/g
 
 export default css => {
-  css.eachAtRule('alias', atRule => {
+  let declarations = {}
+  css.eachAtRule(/^-?let$/, atRule => {
+    console.log(atRule.params)
+
+    let matches
+    while (matches = matchLet.exec(atRule.params)) {
+      let [/*match*/, key, value] = matches
+      declarations[key] = value
+    }
+  })
+  css.eachAtRule(/^-?import$/, atRule => {
     let imports = matchImports.exec(atRule.params)
     if (imports) {
-
+      //console.log(imports)
     } else {
       console.log(atRule.params)
     }
